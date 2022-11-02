00fb18bfe06c03358b72c90b79d331f8d99543f9
Rainbow1990 <Rainbow10222@gmail.com>
2022-11-02 09:24:02 +0300

    Basic JavaScript

---
+++ Build JavaScript Objects.js
@@ -0,0 +1,10 @@
+const myDog = {
+    // Only change code below this line
+    "name": "Sam",
+    "legs": 4,
+    tails: 1,
+    friends: ["Lord", "Cat"]
+  
+  
+    // Only change code above this line
+  };
