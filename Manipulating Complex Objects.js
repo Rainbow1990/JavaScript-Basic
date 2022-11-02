306cdb785baecec0bfda3e4b1529333e9a5b84ad
Rainbow1990 <Rainbow10222@gmail.com>
2022-11-02 14:01:18 +0300

    Basic JavaScript

---
+++ Manipulating Complex Objects.js
@@ -0,0 +1,26 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+  
+    {
+      "artist": "Rainbow",
+      "title": "Long Live Rock 'n' Roll",
+      "release_year": 1978,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+  
+    }
+  ];
